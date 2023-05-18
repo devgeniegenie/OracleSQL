462f1937b2401d6e3dac96712698eebbfce1e988
Genie <devgeniegenie@gmail.com>
2023-05-18 21:36:12 +0900

    6. 테이블 사이를 연결해 주는 조인과 서브 쿼리 8. PL/SQL의 구조와 구성요소

--- oracleSQLAndPLSQL/chap6.sql
+++ oracleSQLAndPLSQL/chap6.sql
@@ -41,3 +41,63 @@ FROM EMPLOYEES a,
 WHERE a.employee_id = b.EMPLOYEE_ID(+)
   AND a.department_id = b.department_id(+);
 
+--ANSI
+--비교
+SELECT a.employee_id, a.emp_name, b.job_id, b.department_id
+FROM EMPLOYEES a,
+     JOB_HISTORY b
+WHERE a.EMPLOYEE_ID = b.EMPLOYEE_ID (+)
+  AND a.DEPARTMENT_ID = b.DEPARTMENT_ID(+);
+
+SELECT a.employee_id, a.emp_name, b.job_id, b.department_id
+FROM EMPLOYEES a
+         LEFT OUTER JOIN JOB_HISTORY b
+             ON (a.EMPLOYEE_ID = b.EMPLOYEE_ID AND a.department_id = b.DEPARTMENT_ID);
+
+--서브쿼리
+--연관성 없는 서브 쿼리
+SELECT COUNT(*)
+FROM employees
+WHERE salary >= (SELECT AVG(salary) FROM employees);
+
+SELECT COUNT(*)
+FROM employees
+WHERE DEPARTMENT_ID IN (SELECT DEPARTMENT_ID FROM departments WHERE PARENT_ID IS NULL);
+
+SELECT employee_id, emp_name, job_id
+FROM employees
+WHERE (EMPLOYEE_ID, job_id) IN (SELECT employee_id, JOB_ID FROM JOB_HISTORY);
+--연관성 있는 서브 쿼리
+SELECT a.department_id, a.department_name
+FROM DEPARTMENTS a
+WHERE EXISTS(SELECT 1 FROM JOB_HISTORY b WHERE a.department_id = b.department_id);
+
+SELECT a.employee_id,
+       (SELECT b.emp_name FROM EMPLOYEES b WHERE a.employee_id = b.EMPLOYEE_ID)              AS emp_name,
+       a.department_id,
+       (SELECT b.department_name FROM departments b WHERE a.department_id = b.DEPARTMENT_ID) AS dep_name
+FROM JOB_HISTORY a;
+
+SELECT a.department_id, a.department_name
+FROM departments a
+WHERE EXISTS(SELECT 1
+             FROM EMPLOYEES b
+             WHERE a.department_id = b.DEPARTMENT_ID
+               AND b.SALARY > (SELECT AVG(SALARY) FROM EMPLOYEES));
+
+SELECT department_id, AVG(SALARY)
+FROM EMPLOYEES a
+WHERE department_id IN (SELECT DEPARTMENT_ID FROM DEPARTMENTS WHERE parent_id = 90)
+GROUP BY department_id;
+
+SELECT a.employee_id, a.emp_name, b.department_id, b.department_name
+FROM EMPLOYEES a,
+     departments b,
+     (SELECT AVG(c.salary) AS avg_salary
+      FROM departments b,
+           employees c
+      WHERE b.PARENT_ID = 90
+        AND b.DEPARTMENT_ID = c.DEPARTMENT_ID) d
+WHERE a.department_id = b.department_id
+  AND a.salary > d.avg_salary;
+
---
+++ oracleSQLAndPLSQL/chap8.sql
@@ -0,0 +1,86 @@
+--PL/SQL ?? ??
+--sqlplus
+SET SERVEROUTPUT ON
+DECLARE
+    vi_num NUMBER;
+BEGIN
+    vi_num := 100;
+    DBMS_OUTPUT.PUT_LINE(vi_num);
+END;
+/
+
+SET SERVEROUTPUT ON
+SET TIMING ON
+DECLARE
+    vi_num NUMBER;
+BEGIN
+    vi_num := 100;
+
+    DBMS_OUTPUT.PUT_LINE(vi_num);
+END;
+/
+
+DECLARE
+    a INTEGER := 2 ** 2 * 3 ** 2;
+BEGIN
+
+    DBMS_OUTPUT.PUT_LINE('a = ' || TO_CHAR(a));
+END;
+/
+
+DECLARE
+    vs_emp_name varchar2(80);
+    vs_dep_name varchar2(80);
+BEGIN
+    SELECT a.emp_name, b.department_name
+    INTO vs_emp_name, vs_dep_name
+    FROM EMPLOYEES a,
+         DEPARTMENTS b
+    WHERE a.DEPARTMENT_ID = b.DEPARTMENT_ID
+      AND a.EMPLOYEE_ID = 100;
+
+    DBMS_OUTPUT.PUT_LINE(vs_emp_name || ' - ' || vs_dep_name);
+END;
+/
+
+DECLARE
+    vs_emp_name EMPLOYEES.emp_name%type;
+    vs_dep_name DEPARTMENTS.department_name%type;
+BEGIN
+    SELECT a.emp_name, b.department_name
+    INTO vs_emp_name, vs_dep_name
+    FROM EMPLOYEES a,
+         DEPARTMENTS b
+    WHERE a.DEPARTMENT_ID = b.DEPARTMENT_ID
+      AND a.EMPLOYEE_ID = 100;
+
+    DBMS_OUTPUT.PUT_LINE(vs_emp_name || ' - ' || vs_dep_name);
+END ;
+/
+
+
+
+CREATE TABLE ch08_varchar2
+(
+    VAR1 varchar2(4000)
+);
+INSERT INTO ch08_varchar2 (VAR1)
+VALUES ('...');
+COMMIT;
+
+select * from ch08_varchar2;
+
+DECLARE
+    vs_sql_vharchar2   varchar2(4000);
+    vs_plsql_vharchar2 varchar2(32767);
+BEGIN
+    --ch08_varchar2 ???? ?? ??? ??
+    SELECT VAR1 into vs_sql_vharchar2 FROM ch08_varchar2;
+
+    --PL/SQL ??? 4000BYTE ?? ??? ?? ??
+    vs_plsql_vharchar2 := vs_sql_vharchar2 || ' - ' || vs_sql_vharchar2 || ' - ' || vs_sql_vharchar2;
+
+    DBMS_OUTPUT.PUT_LINE('SQL_VARCHAR2 ?? : ' || LENGTHB(vs_sql_vharchar2));
+    DBMS_OUTPUT.PUT_LINE('PL_SQL VARCHAR2 ?? : ' || LENGTHB(vs_plsql_vharchar2));
+END;
+/
